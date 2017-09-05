3ebb7e7b63affff2b50f2205c3864c0942c76ae4
Erasmus Bieri <erasmus.bieri@krw.ch>
2017-09-05 10:38:17 +0200

    Erstellen HTML-Seite funktioniert rudimentär

--- UebersichtFilmePhysik.py
+++ UebersichtFilmePhysik.py
@@ -6,6 +6,9 @@ NameFilmverzeichnis = "Filmverzeichnis"
 
 import ModuleFilmphys as mf
 import os
+from xml.dom.minidom import parse
+import xml.dom.minidom
+import hashlib
 
 # Schlussendlich sollen alle Funktionen als Module augelagert werden
 # Das vereinfacht die Wartung und erhöht die Übersicht.
@@ -23,8 +26,11 @@ for root, dirs, files in os.walk(pfadFilmverzeichnis):
 
 ListeMitDateienOhneEndung = []
 ListeXMLDateiMitFilmDateien = []
+ListeXMLDateiMitFilmDateienNameFilm = []
 ListeDateienOhneXML = []
 ListeXMLDateienOhneFilmDateien = []
+
+DatenAlleFilme = []
 
 # diverse Ausgaben
 if debug == 1:
@@ -82,12 +88,17 @@ for root, dirs, files in os.walk(pfadFilmverzeichnis):
                         if DateiUndEndung[0] == DateiUndEndung2[0] and DateiUndEndung[1] != DateiUndEndung2[1]:
                             # weitere Datei vorhanden
                             weitereDateiVorhanden = 1
-                            #if debug:
-                            #    print(DateiUndEndung[0] + "." + DateiUndEndung[1] + "/" + DateiUndEndung2[0] + "." + DateiUndEndung2[1])
-                    if weitereDateiVorhanden == 1:
-                        print("NOTIEREN: " + DateiUndEndung[0] + "." + DateiUndEndung[1] + ": Es ist eine XML-Datei + weitere Datei vorhanden.")
-                        ListeXMLDateiMitFilmDateien.append(root + os.sep + DateiUndEndung[0] + "." + DateiUndEndung[1])
-                    else:
+                            Eintrag = []
+                            print("NOTIEREN: " + DateiUndEndung[0] + "." + DateiUndEndung[1] + ": Es ist eine XML-Datei + weitere Datei vorhanden.")
+                            Eintrag.append(root + os.sep + DateiUndEndung[0] + "." + DateiUndEndung[1])
+                            Eintrag.append(DateiUndEndung2[1])
+                            ListeXMLDateiMitFilmDateien.append(Eintrag)
+
+                    #if weitereDateiVorhanden == 1:
+                    #    print("NOTIEREN: " + DateiUndEndung[0] + "." + DateiUndEndung[1] + ": Es ist eine XML-Datei + weitere Datei vorhanden.")
+                    #    ListeXMLDateiMitFilmDateien.append(root + os.sep + DateiUndEndung[0] + "." + DateiUndEndung[1])
+                    #    ListeXMLDateiMitFilmDateienNameFilm.append(root + os.sep + DateiUndEndung2[0] + "." + DateiUndEndung2[1])
+                    if not weitereDateiVorhanden == 1:
                         print("NOTIEREN: " + DateiUndEndung[0] + "." + DateiUndEndung[1] + ": Es ist NUR eine XML-Datei vorhanden.")
                         ListeXMLDateienOhneFilmDateien.append(root + os.sep + DateiUndEndung[0] + "." + DateiUndEndung[1])
                 else:
@@ -110,11 +121,11 @@ if debug:
     print("")
     print("------------")
     print("*** Übersicht Dateilisten ***")
-    print("Liste mit Dateien ohne Endung:")
-    print(ListeMitDateienOhneEndung)
-    print("")
     print("Liste mit xml-Dateien mit Film:")
     print(ListeXMLDateiMitFilmDateien)
+    print("")
+    print("Liste mit Dateien ohne Endung:")
+    print(ListeMitDateienOhneEndung)
     print("")
     print("Liste mit xml-Dateien ohne Film:")
     print(ListeXMLDateienOhneFilmDateien)
@@ -124,3 +135,100 @@ if debug:
     print("")
     print("------------")
     print("")
+
+# Liste ListeXMLDateiMitFilmDateien durchgehen und Daten aus XML-Dateien auslesen:
+listeMitEintraegen = ['titel', 'beschreibung', 'stichworte','links']
+for datei in ListeXMLDateiMitFilmDateien:
+    print(datei[0])
+    print(datei[0][:-4] + '.' + datei[1])
+    DOMTree = xml.dom.minidom.parse(datei[0])
+    film = DOMTree.documentElement
+    
+    DatenEinzelnerFilm = []
+    for eintrag in listeMitEintraegen:
+        # Falls kein Eintrag vorhanden, gibt es eine Fehlermeldung:
+        if eintrag == 'titel':
+            try:
+                s = film.getElementsByTagName(eintrag)[0].childNodes[0].data
+            except:
+                s = datei[0].split('/')[-1:][0][:-4]
+                #print(s)
+        else:
+            try:
+                s = film.getElementsByTagName(eintrag)[0].childNodes[0].data
+            except:
+                s = ""
+                #print(s)
+        
+        DatenEinzelnerFilm.append(s)
+        
+    #Film-Verzeichnis auslesen:
+    #print("Filmverzeichnis:")
+    #print(datei[len(pfadFilmverzeichnis):-4])
+    DatenEinzelnerFilm.append(datei[0][len(pfadFilmverzeichnis)+1:-4])
+
+    #Hashsumme (sha1sum) Datei bestimmen:
+    with open(datei[0], "rb") as f:
+        data = f.read()
+        sha1summe = hashlib.sha1()
+        sha1summe.update(data)
+        bstr = sha1summe.hexdigest()
+        #print(bstr[:8])
+        
+    DatenEinzelnerFilm.append(bstr[:8])
+
+    #print(DatenEinzelnerFilm)
+
+    DatenAlleFilme.append(DatenEinzelnerFilm)
+
+    print(DatenAlleFilme)
+        
+# HTML-Datei Daten:
+NameHTMLDatei = "UebersichtFilme2.html"
+TitelHTMLDatei = '&Uuml;bersicht Filme'
+AnzahlKategorien = len(DatenAlleFilme[0])
+SpaltenNamen = listeMitEintraegen
+SpaltenNamen.append('verzeichnis')
+SpaltenNamen.append('hash')
+AnzahlSpaltenNamen = len(SpaltenNamen)
+print(str(AnzahlSpaltenNamen) + '/' + str(AnzahlKategorien))
+if AnzahlSpaltenNamen != AnzahlKategorien:
+    print("Die Anzahl Spalten und Anzahl Kategorien stimmt nicht überein, ABRUCH!")
+    exit
+
+Ausgabedatei = open(NameHTMLDatei,"w")
+AnfangHTML = "<!DOCTYPE html>" + '\n' + '<html lang="de">' + '\n' + '<head>' + '\n' + '<meta charset="utf-8">' + '\n' + '<meta name="viewport" content="width=device-width, initial-scale=1.0">' + '\n' + '<title> ' + TitelHTMLDatei + ' </title>' + '\n' + '</head>' + '\n' + '<body>' + '\n'
+Ausgabedatei.write(AnfangHTML)
+Ausgabedatei.write('<h2> ' + TitelHTMLDatei + ' </h2>')
+AnfangTabelle = '<table border="1" width="100%">' + '\n' + '<colgroup>' + '\n'
+s = ""
+for i in range(0,AnzahlSpaltenNamen):
+    s = s + '<col width="1*">'
+AnfangTabelle = AnfangTabelle + '\n' + '</colgroup>' + '\n' + '<tr bgcolor="#EEEEEE">' + '\n'
+Ausgabedatei.write(AnfangTabelle)
+
+MitteTabelle = ""
+for i in range(0,AnzahlSpaltenNamen):
+    #print(i)
+    #print(SpaltenNamen[i])
+    MitteTabelle = MitteTabelle + '<td> <b> ' + SpaltenNamen[i] + '</b> </td>'
+MitteTabelle = MitteTabelle + '\n' + '</tr>' + '\n'
+Ausgabedatei.write(MitteTabelle)
+
+
+print(len(DatenAlleFilme))
+#for i in range(0,2):
+for i in range(len(DatenAlleFilme)):
+    NeueZeile = "<tr>"
+    for j in range(0,AnzahlSpaltenNamen):
+        NeueZeile = NeueZeile + '<td> ' + str(DatenAlleFilme[i][j]) + '</td>'
+    NeueZeile = NeueZeile + '\n' + '</tr>' + '\n'
+    Ausgabedatei.write(NeueZeile)
+
+EndeTabelle = '<!-- usw. andere Zeilen der Tabelle -->' + '\n' + '</table>' + '\n'
+Ausgabedatei.write(EndeTabelle)
+
+EndeHTML = '</body>' + '\n' + '</html>'
+Ausgabedatei.write(EndeHTML)
+Ausgabedatei.close()
+    
